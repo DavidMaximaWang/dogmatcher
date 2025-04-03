743af83e2a3c6984ee39f2adcd8ddda94e4a538e
qunlingwang <qunlingwang@gmail.com>
2025-04-03 11:17:38 -0700

    Some screens to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,9 +11,12 @@ const About = () => {
                 {' '}
                 Code for this project:&nbsp;
                 <a href="https://github.com/DavidMaximaWang/fetch-react" target="_blank" rel="noopener noreferrer">
-                    Fetch React Project
+                    Fetch React Project(private, email: qunlingw@gmail.com)
                 </a>
             </p>
+            <p><img  style={{ maxWidth: '100%', height: 'auto', borderRadius: 8 }} src="https://private-user-images.githubusercontent.com/14916029/430071329-c55ad301-33c9-4813-b592-3fb0df2731ee.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDM3MDE0OTMsIm5iZiI6MTc0MzcwMTE5MywicGF0aCI6Ii8xNDkxNjAyOS80MzAwNzEzMjktYzU1YWQzMDEtMzNjOS00ODEzLWI1OTItM2ZiMGRmMjczMWVlLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA0MDMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNDAzVDE3MjYzM1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWRjYWZkYTBiYWQ3YmQyZWVkMzhmMDUwMWI5YmVmYjI0YzFlNzkyNDlmMDQ0ZDc5ZDM5MjExMzY1YWU2ODc0YTUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.dw_QSQYhlc010BRUTifC4Ci5F5M-stxTSkNdxK7o6qI" alt="screens" /></p>
+            <p><img  style={{ maxWidth: '100%', height: 'auto', borderRadius: 8 }} src="https://private-user-images.githubusercontent.com/14916029/430086058-2e595001-6db9-4914-b3ed-01ea54ce28be.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDM3MDQzNjEsIm5iZiI6MTc0MzcwNDA2MSwicGF0aCI6Ii8xNDkxNjAyOS80MzAwODYwNTgtMmU1OTUwMDEtNmRiOS00OTE0LWIzZWQtMDFlYTU0Y2UyOGJlLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA0MDMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNDAzVDE4MTQyMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTdiNTdkZmVmZmU4ODA1ZWI1NDdkZmIwZWM3MzM4NGExNmQyNWExYTRmMDlmZDQzYTJmMjVlZGU0Njg1MGRjNjgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.yRKqwlDU-VeGlsCNN7KH3rgXrR7cj-eoMGfKAWiB2vA" alt="screens" /></p>
+
         </div>
     );
 };
